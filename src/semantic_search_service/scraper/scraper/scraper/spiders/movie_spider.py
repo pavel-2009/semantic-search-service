ed85799d65600e2a5de6c2a51d454c0364ac3663
Павел <ms.radionova@bk.ru>
2026-08-19 13:58:51 +0500

    modified

--- src/semantic_search_service/scraper/scraper/scraper/spiders/movie_spider.py
+++ src/semantic_search_service/scraper/scraper/scraper/spiders/movie_spider.py
@@ -1,6 +1,6 @@
 """Spider for Kinopoisk search"""
 
-from typing import Any, Generator
+# from typing import Any, Generator
 
 import scrapy
 from scrapy.http import Response
@@ -9,11 +9,9 @@ from scrapy.http import Response
 class MovieScrapy(scrapy.Spider):
     name = "movies"
 
-    def start_requests(self) -> Generator[Any]:
-        yield scrapy.Request(
-            "https://www.kinopoisk.ru/lists/categories/movies/",
-            callback=self.parse
-        )
+    start_urls = [
+        "https://www.kinopoisk.ru/lists/categories/movies/"
+    ]
 
     def parse(self, response: Response):
         print(response.url)
