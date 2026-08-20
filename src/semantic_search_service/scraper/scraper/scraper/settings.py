ac17ae2fa698b836ed9a3979d89dfa14a9fc7bc1
Павел <ms.radionova@bk.ru>
2026-08-20 13:24:40 +0500

    d

--- src/semantic_search_service/scraper/scraper/scraper/settings.py
+++ src/semantic_search_service/scraper/scraper/scraper/settings.py
@@ -59,7 +59,7 @@ DOWNLOAD_DELAY = 1
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   "scraper.pipelines.ScraperPipeline": 300,
+   "scraper.pipelines.JsonPipeline": 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
