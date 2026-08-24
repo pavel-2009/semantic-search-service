eced189229f083cea19d5d0c743b8f576e107e38
Павел <ms.radionova@bk.ru>
2026-08-24 15:12:05 +0500

    created qdrant client

---
+++ src/semantic_search_service/core/qdrant_client.py
@@ -0,0 +1,19 @@
+"""Qdrant singleton client"""
+
+from qdrant_client import QdrantClient
+from src.semantic_search_service.core.config import settings
+
+
+class QdrantClientSingleton:
+    """Qdrant singleton client"""
+
+    _instance: QdrantClient | None = None
+
+    @classmethod
+    def get_client(cls) -> QdrantClient:
+        if cls._instance is None:
+            cls._instance = QdrantClient(
+                host=settings.QDRANT_HOST,
+                port=settings.QDRANT_PORT
+            )
+        return cls._instance
